e0c680cfcf8d5c39381959c740ca2671f4be63f5
TatianaOrtega <tatianaortega92@hotmail.com>
2022-10-12 15:44:56 -0500

    Se agrega comentarios

--- src/numerofactorial/NumeroFactorial.java
+++ src/numerofactorial/NumeroFactorial.java
@@ -1,4 +1,8 @@
-
+/*
+ * Creado por Tatiana Ortega
+ * Fecha 11-09-2022
+ * Calcula un numero factorial
+*/
 package numerofactorial;
 
 import java.util.Scanner;
@@ -9,11 +13,15 @@ public class NumeroFactorial {
    
     public static void main(String[] args) {
         Scanner scan =  new Scanner(System.in);
+        //Solicita ingreso de numero entero
         System.out.println("Ingrese un numero");
         int numero = scan.nextInt();
         
+        //Instacia del objeto factorial
         Factorial factorial = new Factorial();
+        //Llamada al metodo que realiza el factorial
         int resultado = factorial.calcularFactorial(numero);
+        //Muestra en pantalla el numero factorial
         System.out.println("El factorial es: " + resultado);
 
     }
